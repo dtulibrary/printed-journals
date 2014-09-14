ab963edb7ae20cba644e041d03b5e4740f3845ec
franck <franck@cvt.dk>
2014-09-15 01:56:17 +0200

    Added proper indexes and show fields

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -13,6 +13,7 @@ class CatalogController < ApplicationController
 
   configure_blacklight do |config|
     config.default_solr_params = {
+      :qf => 'id_tesim title_tesim title_alternative_tesim issn_tesim publisher_tesim physical_location_place_tesim physical_location_years_tesim physical_location_note_tesim',
       :qt => 'search',
       :rows => 10
     }
@@ -71,20 +72,10 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field solr_name('title', :stored_searchable, type: :string), :label => 'Title:'
-    config.add_show_field solr_name('title_vern', :stored_searchable, type: :string), :label => 'Title:'
-    config.add_show_field solr_name('subtitle', :stored_searchable, type: :string), :label => 'Subtitle:'
-    config.add_show_field solr_name('subtitle_vern', :stored_searchable, type: :string), :label => 'Subtitle:'
-    config.add_show_field solr_name('author', :stored_searchable, type: :string), :label => 'Author:'
-    config.add_show_field solr_name('author_vern', :stored_searchable, type: :string), :label => 'Author:'
-    config.add_show_field solr_name('format', :symbol), :label => 'Format:'
-    config.add_show_field solr_name('url_fulltext_tsim', :stored_searchable, type: :string), :label => 'URL:'
-    config.add_show_field solr_name('url_suppl_tsim', :stored_searchable, type: :string), :label => 'More Information:'
-    config.add_show_field solr_name('language', :stored_searchable, type: :string), :label => 'Language:'
-    config.add_show_field solr_name('published', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_show_field solr_name('published_vern', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_show_field solr_name('lc_callnum', :stored_searchable, type: :string), :label => 'Call number:'
-    config.add_show_field solr_name('isbn', :stored_searchable, type: :string), :label => 'ISBN:'
+    config.add_show_field solr_name('id', :stored_searchable, type: :string), :label => 'id:'
+    config.add_show_field solr_name('title_alternative', :stored_searchable, type: :string), :label => 'Alternative title:'
+    config.add_show_field solr_name('issn', :stored_searchable, type: :string), :label => 'ISSN:'
+    config.add_show_field solr_name('publisher', :stored_searchable, type: :string), :label => 'Publisher:'
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -105,40 +96,82 @@ class CatalogController < ApplicationController
     # since we aren't specifying it otherwise.
 
     config.add_search_field 'all_fields', :label => 'All Fields'
+    config.add_search_field('title', :label => 'Title') do |field|
+        field.solr_local_parameters = {
+            :qf => 'title_tesim',
+            :pf => 'title_tesim',
+        }
+    end
+    config.add_search_field('title_alternative', :label => 'Alternative title') do |field|
+        field.solr_local_parameters = {
+            :qf => 'title_alternative_tesim',
+            :pf => 'title_alternative_tesim',
+        }
+    end
+    config.add_search_field('issn', :label => 'ISSN') do |field|
+        field.solr_local_parameters = {
+            :qf => 'issn_tesim',
+            :pf => 'issn_tesim',
+        }
+    end
+    config.add_search_field('publisher', :label => 'Publisher') do |field|
+        field.solr_local_parameters = {
+            :qf => 'publisher_tesim',
+            :pf => 'publisher_tesim',
+        }
+    end
+    config.add_search_field('place', :label => 'Location') do |field|
+        field.solr_local_parameters = {
+            :qf => 'physical_location_place_tesim',
+            :pf => 'physical_location_place_tesim',
+        }
+    end
+    config.add_search_field('years', :label => 'Years') do |field|
+        field.solr_local_parameters = {
+            :qf => 'physical_location_years_tesim',
+            :pf => 'physical_location_years_tesim',
+        }
+    end
+    config.add_search_field('note', :label => 'Notes') do |field|
+        field.solr_local_parameters = {
+            :qf => 'physical_location_note',
+            :pf => 'physical_location_note',
+        }
+    end
 
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields.
 
-    config.add_search_field('title') do |field|
+#   config.add_search_field('title') do |field|
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
       # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = {
-        :qf => '$title_qf',
-        :pf => '$title_pf'
-      }
-    end
+#     field.solr_local_parameters = {
+#       :qf => '$title_qf',
+#       :pf => '$title_pf'
+#     }
+#   end
 
-    config.add_search_field('author') do |field|
-      field.solr_local_parameters = {
-        :qf => '$author_qf',
-        :pf => '$author_pf'
-      }
-    end
+#   config.add_search_field('author') do |field|
+#     field.solr_local_parameters = {
+#       :qf => '$author_qf',
+#       :pf => '$author_pf'
+#     }
+#   end
 
     # Specifying a :qt only to show it's possible, and so our internal automated
     # tests can test it. In this case it's the same as
     # config[:default_solr_parameters][:qt], so isn't actually neccesary.
-    config.add_search_field('subject') do |field|
-      field.qt = 'search'
-      field.solr_local_parameters = {
-        :qf => '$subject_qf',
-        :pf => '$subject_pf'
-      }
-    end
+#   config.add_search_field('subject') do |field|
+#     field.qt = 'search'
+#     field.solr_local_parameters = {
+#       :qf => '$subject_qf',
+#       :pf => '$subject_pf'
+#     }
+#   end
 
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
