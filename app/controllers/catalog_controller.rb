9b851bd547ea7cb966efba2073db252c7495d226
franck <franck@cvt.dk>
2015-03-10 12:59:43 +0100

    Finalised search

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -16,8 +16,64 @@ class CatalogController < ApplicationController
             :limit => 25
         }
     }
+    config.default_solr_params = {
+      :qf => 'id_tesim title_tesim title_alternative_tesim title_previous_tesim title_next_tesim issn_tesim coden_tesim publisher_tesim notes_tesim holdings_tesim physical_location_place_tesim physical_location_years_tesim physical_location_note_tesim',
+      :qt => 'search',
+      :rows => 10
+    }
+    Rails.logger.info "config.class: #{config.class}"
+    Rails.logger.info "config.methods: #{config.methods.sort}"
+    Rails.logger.info "config: #{config.inspect}"
+    config.search_fields = {};
+    config.add_search_field('all_fields', :label => 'All Fields') do |field|
+        field.solr_local_parameters = {
+            :qf => 'id title_tesim title_alternative_tesim title_previous_tesim title_next_tesim issn_tesim coden_tesim publisher_tesim notes_tesim holdings_tesim physical_location_place_tesim physical_location_years_tesim physical_location_note_tesim organisation_tesim',
+            :pf => 'title_tesim',
+        }
+    end
+    config.add_search_field('title', :label => 'Title') do |field|
+        field.solr_local_parameters = {
+            :qf => 'title_tesim',
+            :pf => 'title_tesim',
+        }
+    end
+    config.add_search_field('title_alternative', :label => 'Alternative title') do |field|
+        field.solr_local_parameters = {
+            :qf => 'title_alternative_tesim title_previous_tesim title_next_tesim',
+            :pf => 'title_alternative_tesim',
+        }
+    end
+    config.add_search_field('issn', :label => 'ISSN') do |field|
+        field.solr_local_parameters = {
+            :qf => 'issn_tesim',
+            :pf => 'issn_tesim',
+        }
+    end
+    config.add_search_field('publisher', :label => 'Publisher') do |field|
+        field.solr_local_parameters = {
+            :qf => 'publisher_tesim',
+            :pf => 'publisher_tesim',
+        }
+    end
+    config.add_search_field('place', :label => 'Location') do |field|
+        field.solr_local_parameters = {
+            :qf => 'physical_location_place_tesim',
+            :pf => 'physical_location_place_tesim',
+        }
+    end
+    config.add_search_field('years', :label => 'Years') do |field|
+        field.solr_local_parameters = {
+            :qf => 'physical_location_years_tesim',
+            :pf => 'physical_location_years_tesim',
+        }
+    end
+    config.add_search_field('note', :label => 'Notes') do |field|
+        field.solr_local_parameters = {
+            :qf => 'physical_location_note_tesim',
+            :pf => 'physical_location_note_tesim',
+        }
+    end
   end
-
   # get search results from the solr index
   # Overriding this method in order to enable rendering of OAI-PMH responses when .oaipmh is the requested format
   def index
@@ -36,6 +92,4 @@ class CatalogController < ApplicationController
       document_export_formats(format)
     end
   end
-
-
 end
