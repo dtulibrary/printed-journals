b53138f762bea78aa9c0395d6ea69ccd7f213bb6
Jimmy Petersen <jipe@dtu.dk>
2017-06-07 13:24:13 +0200

    Make auth path configurable

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module Missingstuff
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.riyosha_url = 'https://auth.findit.dtu.dk'
   end
 end
--- config/initializers/dtu_rails_common.rb
+++ config/initializers/dtu_rails_common.rb
@@ -3,8 +3,8 @@ module DtuRailsCommon
     config.app_name = 'DTU Findit - Printed Journals'
     config.dtu_font_enabled = Rails.application.config.try(:dtu_common_layout) && Rails.application.config.dtu_common_layout[:dtu_font_enabled]
     config.auth = {
-      :cas_url => 'http://staging.auth.cvt.dk/users',
-      :api_url => 'http://staging.auth.cvt.dk/rest',
+      :cas_url => "#{Rails.application.config.riyosha_url}/users",
+      :api_url => "#{Rails.application.config.riyosha_url}/rest",
       :ip => {
         :walk_in  => [],
         :campus   => [],
